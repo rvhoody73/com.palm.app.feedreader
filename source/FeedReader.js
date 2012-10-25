3a6ba412bb89b9e131b94013be1eb05741985b7b
rvhoody73 <rvhoody73@gmail.com>
2012-10-25 16:56:16 -0700

    Working with Webos Desktop build

---
+++ source/FeedReader.js
@@ -0,0 +1,16 @@
+enyo.kind({
+  name: "MyApps.FeedReader",
+  kind: enyo.VFlexBox,
+  components: [
+      {kind: "PageHeader", content: "Enyo FeedReader"},
+      {kind: "RowGroup", caption: "Feed URL", components: [
+          {kind: "InputBox", components: [
+              {name: "feedUrl", kind: "Input", flex: 1},
+              {kind: "Button", caption: "Get Feed", onclick: "btnClick"}
+          ]}
+      ]}
+  ],
+  btnClick: function() {
+      // handle the button click
+  }
+});
